1be3079c78f037e64e4a0d05ecc7247a418853a9
Mathias Brodala <mbrodala@pagemachine.de>
2020-09-01 16:54:34 +0200

    [FEATURE] Add optimized XLSX connector
    This connector uses a different library which is highly optimized for
    low memory usage. Since it only covers XSLX, not classic XLS, we need
    two separate connectors.

--- Tests/Functional/Service/XlsxConnectorTest.php
+++ Tests/Functional/Service/XlsxConnectorTest.php
@@ -9,9 +9,9 @@ use Nimut\TestingFramework\TestCase\FunctionalTestCase;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
- * Testcase for \Pagemachine\SvconnectorXls\Service\XlsConnector
+ * Testcase for \Pagemachine\SvconnectorXls\Service\XlsxConnector
  */
-final class XlsConnectorTest extends FunctionalTestCase
+final class XlsxConnectorTest extends FunctionalTestCase
 {
     protected $testExtensionsToLoad = [
         'typo3conf/ext/svconnector',
@@ -19,7 +19,7 @@ final class XlsConnectorTest extends FunctionalTestCase
     ];
 
     protected $pathsToLinkInTestInstance = [
-        'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xls' => 'fileadmin/plain.xls',
+        'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xlsx' => 'fileadmin/plain.xlsx',
     ];
 
     /**
@@ -37,32 +37,32 @@ final class XlsConnectorTest extends FunctionalTestCase
 
     public function validCases(): \Generator
     {
-        yield 'plain XLS file' => [
+        yield 'plain XLSX file' => [
             [
-                'filename' => 'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xls',
+                'filename' => 'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xlsx',
             ],
             [
                 [
-                    'A' => 'Header 1',
-                    'B' => 'Header 2',
-                    'C' => 'Header 3',
+                    0 => 'Header 1',
+                    1 => 'Header 2',
+                    2 => 'Header 3',
                 ],
                 [
-                    'A' => 'Value 1.1',
-                    'B' => 'Value 1.2',
-                    'C' => 'Value 1.3',
+                    0 => 'Value 1.1',
+                    1 => 'Value 1.2',
+                    2 => 'Value 1.3',
                 ],
                 [
-                    'A' => 'Value 2.1',
-                    'B' => 'Value 2.2',
-                    'C' => 'Value 2.3',
+                    0 => 'Value 2.1',
+                    1 => 'Value 2.2',
+                    2 => 'Value 2.3',
                 ],
             ],
         ];
 
-        yield 'XLS file, respecting headers' => [
+        yield 'XLSX file, respecting headers' => [
             [
-                'filename' => 'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xls',
+                'filename' => 'typo3conf/ext/svconnector_xls/Tests/Functional/Service/Fixtures/plain.xlsx',
                 'skip_rows' => 1,
             ],
             [
@@ -79,9 +79,9 @@ final class XlsConnectorTest extends FunctionalTestCase
             ],
         ];
 
-        yield 'XLS file, relative path' => [
+        yield 'XLSX file, relative path' => [
             [
-                'filename' => 'fileadmin/plain.xls',
+                'filename' => 'fileadmin/plain.xlsx',
                 'skip_rows' => 1,
             ],
             [
@@ -103,6 +103,6 @@ final class XlsConnectorTest extends FunctionalTestCase
     {
         $connectorRepository = GeneralUtility::makeInstance(ConnectorRepository::class);
 
-        return $connectorRepository->findServiceByKey('tx_svconnectorxls_xls');
+        return $connectorRepository->findServiceByKey('tx_svconnectorxls_xlsx');
     }
 }
