aef79f2326bba5eecc99e92bb1f52c71f421ec60
Mathias Brodala <mbrodala@pagemachine.de>
2024-03-19 08:53:07 +0100

    [TASK] Fix PHP code style violations

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,17 +1,17 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-  'title' => 'Connector service - XLS(X)',
-  'description' => 'Connector service for reading a XLS(X) file',
-  'category' => 'misc',
-  'author' => 'Mathias Brodala',
-  'author_email' => 'mbrodala@pagemachine.de',
-  'author_company' => 'Pagemachine AG',
-  'state' => 'stable',
-  'version' => '3.1.0',
-  'constraints' => [
-    'depends' => [
-      'typo3' => '11.5.0-12.4.99',
+    'title' => 'Connector service - XLS(X)',
+    'description' => 'Connector service for reading a XLS(X) file',
+    'category' => 'misc',
+    'author' => 'Mathias Brodala',
+    'author_email' => 'mbrodala@pagemachine.de',
+    'author_company' => 'Pagemachine AG',
+    'state' => 'stable',
+    'version' => '3.1.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '11.5.0-12.4.99',
+        ],
     ],
-  ],
 ];
